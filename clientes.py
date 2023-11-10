b88c483848d00607fcaa22c20be918ca0bc98d0d
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-11-10 01:47:09 -0300

    add fake data function

--- clientes.py
+++ clientes.py
@@ -1,2 +1,13 @@
 import pandas as pd
 import faker import Faker
+
+faker = Faker('es_CL')
+
+def generar_cliente():
+  nombre = fake.name()
+  correo = fake.email()
+  telefono = fake_phone_number()
+  direccion = fake.address()
+
+  return {"Nombre": nombre, "Correo": correo, "Telefono": telefono, "Direccion": direccion}
+
