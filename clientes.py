70bb849b4a5c808c56c7c054d8a29082734e7b61
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-11-10 02:01:15 -0300

    fix typo errors

--- clientes.py
+++ clientes.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from faker import Faker
 
-faker = Faker('es_CL')
+fake = Faker('es_CL')
 
 def generar_cliente():
   nombre = fake.name()
   correo = fake.email()
-  telefono = fake_phone_number()
+  telefono = fake.phone_number()
   direccion = fake.address()
 
   return {"Nombre": nombre, "Correo": correo, "Telefono": telefono, "Direccion": direccion}
