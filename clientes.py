00c874cd4d084850668078beea890139ff625bb3
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-11-16 19:53:53 -0300

    add company and delete no relevant columns

--- clientes.py
+++ clientes.py
@@ -7,12 +7,12 @@ def generar_cliente():
   nombre = fake.name()
   correo = fake.email()
   telefono = fake.phone_number()
-  direccion = fake.address()
+  company = fake.company()
   ciudad = fake.city()
 
-  return {"Nombre": nombre, "Correo": correo, "Telefono": telefono, "Ciudad": ciudad,"Direccion": direccion}
+  return {"Nombre": nombre, "Correo": correo, "Telefono": telefono,"Nombre de la empresa": company}
 
-num_clientes = 100 #Cantidad de iteraciones (filas) de datos
+num_clientes = 20 #Cantidad de iteraciones (filas) de datos
 clientes = [generar_cliente() for _ in range (num_clientes)]
 
 df = pd.DataFrame(clientes)
